7e87881151f77b4e78bcf7a80a5d32d7ffdac70f
obearbaby <cwz931214@gmail.com>
2016-09-28 00:38:41 -0400

    refine navi script

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+	public Camera camera;
+	bool Click;
+	NavMeshAgent agent;
+	public Transform objectHit;
+	public ArrayList agents;
+
+	void Start()
+	{
+		Click = false;
+		agents = new ArrayList();
+
+	}
+	void Update()
+	{
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			objectHit = hit.transform;
+			if (Input.GetMouseButtonDown(0))
+			{
+				//selecting agents
+				if (objectHit.tag.ToString().Equals("Agent"))
+				{
+					print("Agent selected");
+					objectHit.gameObject.GetComponent<testitem> ().isNowSelected ();
+					agents.Add(objectHit.gameObject);
+					Click = true;
+				}
+				else if (Click == true)
+				{
+					print("Deploying!");
+					foreach (GameObject obj in agents)
+					{
+						obj.GetComponent<testitem>().Goto(hit);
+						obj.GetComponent<testitem>().enabled = true;
+						obj.GetComponent<testitem> ().isNowNotSelected ();
+					}
+					agents.Clear();
+					Click = false;
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/testitem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testitem : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	void Update () 
+	{
+//		if(Input.GetMouseButtonDown (0))
+//		{
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//			RaycastHit hit;
+//
+//			if(Physics.Raycast(ray, out hit))
+//			{
+//				Goto(hit);
+//			}
+//		}
+
+	}
+
+
+	public void isNowSelected() {
+		Renderer rend = GetComponent<Renderer> ();
+		rend.material.color = Color.grey;
+	}
+		
+	public void isNowNotSelected() {
+		Renderer rend = GetComponent<Renderer> ();
+		rend.material.color = Color.white;
+	}
+
+	public void Goto(RaycastHit hit)
+	{
+		agent.SetDestination (hit.point);
+	}
+
+
+}

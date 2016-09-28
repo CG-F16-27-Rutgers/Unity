207ccd5dc704f686d5a386c2b1298d3a0225f0be
jianqin <414119772@qq.com>
2016-09-27 21:58:24 -0400

    add obstacle controller

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    bool select;
+	// Use this for initialization
+	void Start () {
+        select = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0)&&select==false)
+            {
+                if (hit.transform.gameObject.Equals(this.gameObject))
+                {
+                    select = true;
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && select == true && !hit.transform.gameObject.Equals(this.gameObject)) 
+        {
+            select = false;
+        }
+        if (select)
+        {
+            print("123");
+            select = true;
+            rb = this.gameObject.GetComponent<Rigidbody>();
+            float moveHorizontal = 0;
+            float moveVertical = 0;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveVertical = 1;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveVertical = -1;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveHorizontal = -1;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveHorizontal = 1;
+            }
+            Vector3 move = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+            rb.AddForce(move);
+        }
+	}
+}

f655d1d7fea0154ceea2f8509325dbad7a6013b7
jianqin <414119772@qq.com>
2016-10-11 14:58:59 -0400

    original version

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float zmove = Input.GetAxis("Vertical");
+        float xmove = Input.GetAxis("Horizontal");
+		anim.SetFloat("zmove", zmove);
+		anim.SetFloat("xmove", xmove);
+        
+		if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("run", true);
+        }
+        else
+        {
+            anim.SetBool("run", false);
+        }
+        if (Input.GetKey(KeyCode.LeftControl) && xmove!=0)
+        {
+            anim.SetBool("strafe", true);
+        }
+        else
+        {
+            if (zmove == 0 && xmove != 0)
+            {
+                transform.Rotate(new Vector3(0, xmove * 60 * Time.deltaTime));
+            }
+            anim.SetBool("strafe", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("jump");
+        }
+
+    }
+}
